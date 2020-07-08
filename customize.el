1de483faaa9580a6e5862a22afce1d5f1d10019e
Giles Paterson <giles.paterson@blackpepper.co.uk>
2020-07-08 16:39:05 +0100

    adds markdown, magit, git-gutter and removes toolbar

--- customize.el
+++ customize.el
@@ -3,7 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (toc-org use-package))))
+ '(git-gutter:update-interval 2)
+ '(package-selected-packages
+   (quote
+    (git-gutter magit markdown-mode toc-org use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
